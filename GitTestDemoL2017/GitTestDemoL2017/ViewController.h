6f445c80acaaee6648f018ffb271564f15f06a41
zzjpeter <zzj908172@gmail.com>
2017-10-30 14:04:44 +0800

    创建工程

---
+++ GitTestDemoL2017/GitTestDemoL2017/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTestDemoL2017
+//
+//  Created by zhuzhijia on 2017/10/30.
+//  Copyright © 2017年 zhuzhijia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTestDemoL2017/GitTestDemoL2017/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTestDemoL2017
+//
+//  Created by zhuzhijia on 2017/10/30.
+//  Copyright © 2017年 zhuzhijia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
